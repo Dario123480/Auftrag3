268aa4d93f5af51e4c31b5a187df20073a2329f1
ny-dario-z <dario.zueger1@edu.tbz.ch>
2026-03-30 09:29:02 +0200

    added optimization

---
+++ minify.js
@@ -0,0 +1,47 @@
+const minify = require('html-minifier').minify;
+const fs = require('fs-extra');
+const { rimrafSync } = require('rimraf');
+
+const dist = './dist';
+const files = [
+    'index.html', 
+    'about.html', 
+    'produkte.html', 
+    'login.html', 
+    'password-forgot.html', 
+    'contact.html'
+];
+
+async function runTask() {
+    if (fs.existsSync(dist)) {
+        rimrafSync(dist);
+    }
+    fs.mkdirSync(dist);
+
+    if (fs.existsSync('./assets')) {
+        fs.copySync('./assets', './dist/assets');
+    }
+
+    if (fs.existsSync('style.css')) {
+        const css = fs.readFileSync('style.css', 'utf8');
+        const minifiedCss = css.replace(/\s+/g, ' ').replace(/\/\*.*?\*\//g, '');
+        fs.writeFileSync(`${dist}/style.css`, minifiedCss);
+    }
+
+    files.forEach(file => {
+        if (fs.existsSync(file)) {
+            const content = fs.readFileSync(file, 'utf8');
+            const result = minify(content, {
+                collapseWhitespace: true,
+                removeComments: true,
+                minifyCSS: true,
+                removeAttributeQuotes: true
+            });
+            fs.writeFileSync(`${dist}/${file}`, result);
+        }
+    });
+
+    console.log("Build fertig in /dist");
+}
+
+runTask();
